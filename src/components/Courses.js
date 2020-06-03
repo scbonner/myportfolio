dff18cd90bcc706d0e12833b1b62a2ec42d8ff6e
scbonner <codingbootcamp918@gmail.com>
2020-06-03 01:43:18 -0400

    "updates"

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -7,8 +7,8 @@ const Courses = () => {
       <div className="container">
         <div className="row">
           <div className="col m6">
-            <h3>Bogus Studio</h3>
-            <p>We are a studio that aims to give our clients a platform that exemplifies a good user experience...</p>
+            <h3>Project Studio</h3>
+            <p>We have provided in the adjacent column a listing of projects for our viewing.</p>
           </div>
           <div className="col m6">
             <div className="stats">
@@ -19,6 +19,14 @@ const Courses = () => {
               <div className="stat blue darken-2">
                 <h2 className="counter white-text">123</h2>
                 <p className="grey-text darken-1">fresh pots of coffee</p>
+              </div>
+              <div className="stat blue darken-2">
+                <h2 className="counter white-text">123</h2>
+                <p className="grey-text lighten-1">pounds gnar shredded</p>
+              </div>
+              <div className="stat blue darken-2">
+                <h2 className="counter white-text">0</h2>
+                <p className="grey-text darken-1">honeybadgers harmed</p>
               </div>
               <div className="stat blue darken-2">
                 <h2 className="counter white-text">123</h2>
