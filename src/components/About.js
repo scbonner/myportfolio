b7828f428d56da7a161481d11da0ffd0688e952e
scbonner <codingbootcamp918@gmail.com>
2020-06-16 22:41:47 -0400

    "about page is 90% complete"

--- src/components/About.js
+++ src/components/About.js
@@ -14,20 +14,19 @@ const About = () => {
       <br></br>
       <br></br>
       <div class="container">
-        {/* <h1>My Story</h1> */}
         <div id="col">
-          <p> About four years ago, I had a great opportunity to supervise and mentor a student studying computer science at the university in which I worked. My department had nonprofit status, solely dependent upon outside funding though grants.</p>
-          <p>One of my tasks was to maintain and update a statewide collaborative website that included the Juvenile Justice, Mental Health and Substance Use organizations and other non-government agencies. As the collaborative expanded, so did the need to add more pages and subliminal asserts to learn how within a time-sensitive slot of time. My digital capacity was nonexistent other than the normal UI experience.</p>
-          <p>I suggested an idea to partner with the university's computer science department. We would utilize the services of a student needing volunteer service hours. I presented it as a win-win effort. Several advantages, money saved, student would receive volunteer service hours including letter of recommendation, and a quicker turnaround in updating the website.</p>
-          <p>The group agreed, and I had the chance to supervise and mentor a student. Quickly, I became intrigued with the of the field. He would encourage me to transition over in the field.</p>
-          <p>Long story short, his services came to an end. The partnership saved our department somewhere between 5K to 10K dollars, shortened man-hours, student received volunteer hours with a letter of recommendation.</p>
-          <p>And, me? Well, I did go back to school for Coding to become a Software Engineer.</p>
+          <p> About four years ago, I had the awesome opportunity to supervise and mentor a student studying computer science at the university where I worked. The department had a nonprofit status. It was solely financed and dependent upon outside funding sources though grants or government agencies.</p>
+          <p>One of my tasks was to maintain and update a statewide website that included a collaboration with the Juvenile Justice, Mental Health and Substance Use organizations including other non-government agencies. As this collective partnership expanded, so did the need to improve the website. At that time, my technical intellectual capacity was reduced to that of a user.</p>
+          <p>I submitted the idea for the department to partner with the university's computer science department. It was a win-win chance to utilize an internal resource that of a student needing volunteer-service hours.Several advantages, I noted in advance: monies saved, receipt of volunteer-service hours including a letter of recommendation, and the ability to address time sensitive deadlines in keeping the information current.</p>
+          <p>Upper management agreed, and I made all the contacts to supervise and mentor a seasoned student. It is then, I quickly became intrigued with the possibility of transitioning into a different field.</p>
+          <p>Long story short, his services came to an end. The partnership and his efforts saved our department somewhere between 5K to 10K dollars, man-hours cut in half, student received service hour certifiation and a letter of recommendation.</p>
+          <p>And, me? Well, I did go back to school for Coding to become a Software Engineer. It was through his encouragement and patience I jumped out on faith to, again, challenge myself.</p>
         </div>
         </div>
       </div>   
       </div>   
     <div className="container">
-        <h1>Fellowships</h1>
+        <h2>Fellowships</h2>
         <div className="col-fellow">
         <div>
         <p>2016 Youth Justice Leadership Institute - NJJN (2016/Cohort)</p>
